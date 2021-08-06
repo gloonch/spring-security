1b33c645ce90fbf9ddc851bc35d56ecaeac01138
mahdi hadian <mahdihadian@hotmail.com>
2021-08-06 16:46:19 +0430

    Service typo fixed

--- src/main/java/com/fourthmonth/springsecurity/auth/FakeApplicationUserDaoService.java
+++ src/main/java/com/fourthmonth/springsecurity/auth/FakeApplicationUserDaoService.java
@@ -1,6 +1,7 @@
 package com.fourthmonth.springsecurity.auth;
 
 import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +15,7 @@ public class FakeApplicationUserDaoService implements ApplicationUserDao {
 
     private final PasswordEncoder passwordEncoder;
 
+    @Autowired
     public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
