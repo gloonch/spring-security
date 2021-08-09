36b31e2f5c073371455c996fa3879bc7e2463321
mahdi hadian <mahdihadian@hotmail.com>
2021-08-09 18:48:36 +0430

    clean code done

--- src/main/java/com/fourthmonth/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/fourthmonth/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,13 +1,12 @@
 package com.fourthmonth.springsecurity.security;
 
 import com.fourthmonth.springsecurity.auth.ApplicationUserService;
+import com.fourthmonth.springsecurity.jwt.JwtConfig;
 import com.fourthmonth.springsecurity.jwt.JwtTokenVerifier;
 import com.fourthmonth.springsecurity.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,16 +14,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import java.util.concurrent.TimeUnit;
+import javax.crypto.SecretKey;
 
-import static com.fourthmonth.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.fourthmonth.springsecurity.security.ApplicationUserRole.*;
 
 
@@ -36,12 +29,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
     private final ApplicationUserService applicationUserService;
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
+
 
     @Autowired
     public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
-                                     ApplicationUserService applicationUserService) {
+                                     ApplicationUserService applicationUserService, SecretKey secretKey, JwtConfig jwtConfig) {
         this.passwordEncoder = passwordEncoder;
         this.applicationUserService = applicationUserService;
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
     }
 
 
@@ -52,49 +50,13 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
-                .addFilterAfter(new JwtTokenVerifier(), JwtUsernameAndPasswordAuthenticationFilter.class)
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+                .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-//                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name()) // Order does matter
                 .anyRequest().authenticated();
     }
-
-//    @Override
-//    @Bean
-//    protected UserDetailsService userDetailsService() {
-//        UserDetails annaSmithUser = User.builder()
-//                .username("annasmith")
-//                .password(passwordEncoder.encode("password"))
-////                .roles(STUDENT.name()) // ROLE_STUDENT
-//                .authorities(STUDENT.getGrantedAuthorities())
-//                .build();
-//
-//        UserDetails lindaUser = User.builder()
-//                .username("linda")
-//                .password(passwordEncoder.encode("password123"))
-////                .roles(ADMIN.name()) // ROLE_ADMIN
-//                .authorities(ADMIN.getGrantedAuthorities())
-//                .build();
-//
-//        UserDetails tomUser = User.builder()
-//                .username("tom")
-//                .password(passwordEncoder.encode("password123"))
-////                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
-//                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(
-//                annaSmithUser,
-//                lindaUser,
-//                tomUser
-//        );
-//
-//    }
 
 
     @Override
